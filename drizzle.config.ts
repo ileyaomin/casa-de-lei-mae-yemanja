fb3d73682d6e35458214dee3df7914bd0845f822
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-10-22 14:14:18 -0300

    🚚 Move files to path internal

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "./env";
 
 export default defineConfig({
-  schema: "./app/api/database/schema.ts",
-  out: "./app/api/database/.migrations",
+  schema: "./app/api/internal/database/schema.ts",
+  out: "./app/api/internal/database/.migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: env.POSTGRES_URL,
