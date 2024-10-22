dd65b9da2519eac54be2f2d1ecdff240a85daf71
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-10-22 14:20:30 -0300

    ✨ Create migration

---
+++ app/api/internal/database/.migrations/0006_small_harpoon.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS "passes" (
+	"id" text PRIMARY KEY NOT NULL,
+	"gira_id" text NOT NULL,
+	"assistance_id" text NOT NULL,
+	"sons_id" text NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "passes" ADD CONSTRAINT "passes_gira_id_giras_id_fk" FOREIGN KEY ("gira_id") REFERENCES "public"."giras"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "passes" ADD CONSTRAINT "passes_assistance_id_assistance_id_fk" FOREIGN KEY ("assistance_id") REFERENCES "public"."assistance"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "passes" ADD CONSTRAINT "passes_sons_id_sons_id_fk" FOREIGN KEY ("sons_id") REFERENCES "public"."sons"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
