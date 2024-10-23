cd32e5f680a091e4ebaf27bbba9ceaf30ffbb72b
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-10-22 23:55:44 -0300

    ✨ Create and get sons on form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ReactQueryProvider } from "@/providers/reactQuery";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased dark`}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,65 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  async function getSons(): Promise<{ id: string; name: string }[]> {
+    return await fetch("/api/sons").then((res) => res.json());
+  }
+
+  const sonsFetch = useQuery({
+    queryKey: ["get_sons"],
+    queryFn: getSons,
+  });
+
+  return (
+    <Dialog>
+      <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <span className="text-lg font-semibold capitalize">Giras</span>
+
+          <DialogTrigger asChild>
+            <Button>Cadastrar gira</Button>
+          </DialogTrigger>
+        </div>
+      </div>
+
+      <DialogContent className="top-[20%]">
+        <DialogHeader>
+          <DialogTitle>Nova gira</DialogTitle>
+        </DialogHeader>
+
+        <form>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Filho" />
+            </SelectTrigger>
+
+            <SelectContent>
+              {sonsFetch.data?.map((son) => (
+                <SelectItem key={son.id} value={son.id}>
+                  {son.name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 }
