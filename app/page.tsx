588fef44b88233190bf6712e2a54104256a5a4b5
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-10-23 16:47:41 -0300

    🚧 Layout and validate inputs

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,15 @@
-"use client"
-
+import { FormGira } from "@/components/form-gira";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  async function getSons(): Promise<{ id: string; name: string }[]> {
-    return await fetch("/api/sons").then((res) => res.json());
-  }
-
-  const sonsFetch = useQuery({
-    queryKey: ["get_sons"],
-    queryFn: getSons,
-  });
-
   return (
     <Dialog>
       <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
@@ -42,23 +25,10 @@ export default function Home() {
       <DialogContent className="top-[20%]">
         <DialogHeader>
           <DialogTitle>Nova gira</DialogTitle>
+					<DialogDescription>Selecione um filho de santo e a gira de hoje</DialogDescription>
         </DialogHeader>
 
-        <form>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Filho" />
-            </SelectTrigger>
-
-            <SelectContent>
-              {sonsFetch.data?.map((son) => (
-                <SelectItem key={son.id} value={son.id}>
-                  {son.name}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </form>
+        <FormGira />
       </DialogContent>
     </Dialog>
   );
---
+++ components/form-gira.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Button } from "./ui/button";
+import { DialogClose } from "./ui/dialog";
+import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const girasType = z.enum([
+  "Boiadeiro",
+  "Baianos",
+  "Erê",
+  "Marinheiro",
+  "Preto velho",
+  "Esquerda - Exu",
+  "Esquerda - Pomba Gira",
+], {
+	message: "Linha da gira é obrigatória"
+});
+
+const fieldsFormSchema = z.object({
+  name: girasType,
+  responsible_id: z.string({
+		required_error: "Informe um filho responsável pela lista"
+	}),
+});
+
+type FieldsFormSchema = z.infer<typeof fieldsFormSchema>;
+
+export function FormGira() {
+  const { control, handleSubmit, formState } = useForm<FieldsFormSchema>({
+    resolver: zodResolver(fieldsFormSchema),
+  });
+
+  async function getSons(): Promise<{ id: string; name: string }[]> {
+    return await fetch("/api/sons").then((res) => res.json());
+  }
+
+  async function onSubmit(data: FieldsFormSchema) {
+    console.log(data);
+  }
+
+  const sonsFetch = useQuery({
+    queryKey: ["get_sons"],
+    queryFn: getSons,
+  });
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        control={control}
+        name="responsible_id"
+        render={({ field }) => {
+          return (
+            <Select onValueChange={field.onChange} value={field.value}>
+              <SelectTrigger>
+                <SelectValue placeholder="Filho" />
+              </SelectTrigger>
+
+              <SelectContent>
+                {sonsFetch.data?.map((son) => (
+                  <SelectItem key={son.id} value={son.id}>
+                    {son.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          );
+        }}
+      />
+      {formState.errors.responsible_id && (
+        <p className="text-red-400 text-sm">
+          {formState.errors.responsible_id.message}
+        </p>
+      )}
+
+      <Controller
+        control={control}
+        name="name"
+        render={({ field }) => {
+          return (
+            <Select onValueChange={field.onChange} value={field.value}>
+              <SelectTrigger>
+                <SelectValue placeholder="Linha" />
+              </SelectTrigger>
+
+              <SelectContent>
+                {Object.entries(girasType.Values).map(([gira]) => (
+                  <SelectItem key={gira} value={gira}>
+                    {gira}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          );
+        }}
+      />
+      {formState.errors.name && (
+        <p className="text-red-400 text-sm">
+          {formState.errors.name.message}
+        </p>
+      )}
+
+      <div className="flex flex-1 flex-col gap-2 pt-4">
+        <Button type="submit" className="flex-1">
+          Cadastrar
+        </Button>
+        <DialogClose asChild>
+          <Button type="button" variant="ghost" className="flex-1">
+            Cancelar
+          </Button>
+        </DialogClose>
+      </div>
+    </form>
+  );
+}
